b90bec09e487b993636f0c24976c23b3abd06293
awalikar886 <awalikar886@gmail.com>
2025-10-24 10:02:35 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,7 +1,7 @@
 age=int(input("enter your age"))
-if age<0;
+if age<0
 print("age can't be neagative ")
-elif age<18;
+elif age<18
 print("you are not eligible to vote")
 else:
 print("eligible to vote")
